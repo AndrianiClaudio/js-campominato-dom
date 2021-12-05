7f1a92ae4c1d415a578f83a03037fc623664c635
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-12-05 09:30:03 +0100

    salvataggio esercizio corretto.

--- js/script.js
+++ js/script.js
@@ -5,17 +5,45 @@
 // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. : bomba:
 // I numeri nella lista delle bombe non possono essere duplicati.
 // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+
+// BONUS:
+// 1 - quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// 2 - quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+/**
+ * 
+ * @param {*} my_this this,indica lo square cliccato.
+ * @param {*} bombs_index array contentente indici in cui sono presenti le nostre bombe.
+ * @param {*} clicked_index array contente indici square cliccati. 
+ * @returns div che stampa il punteggio e con tutte le bombe visualizzate.
+ */
+function endGame(my_this,bombs_index,clicked_index,msg) {
+    const divScore = createDiv('div-score');
+    // my_this.classList.add('bomb');
+    const squareAll = document.querySelectorAll('.square');
+    for(let j=0;j<squareAll.length;j++) {
+        squareAll[j].classList.add('pointer-events-none');
+        if(bombs_index.includes(j)) {
+            squareAll[j].classList.add('bomb');
+        }
+    }
+    divScore.innerHTML = msg + clicked_index.length;
+    // stampa punteggio
+    return divScore;
+}
+/**
+ * 
+ * @param {*} row #righe
+ * @param {*} col #colonne
+ */
 function printGrid(row, col) {
     let dim = row * col;
-    // acquissco container
+    // acquisisco container
     const container = document.querySelector('.container');
     //rimuovo ogni possibile griglia precedente, se é presente
     container.innerHTML = '';
-
     const clicked_index = [];
     let bombs_index = generateBomb();
-    console.log(bombs_index);
-
+    let clicked_index_max_length = dim - bombs_index.length;
     for (let i = 0; i < dim; i++) {
         //creo il mio square
         const square = document.createElement('div');
@@ -26,24 +54,29 @@ function printGrid(row, col) {
         square.innerHTML = i + 1;
         // click su cella
         square.addEventListener('click', function () {
-            // se numero presente in lista generati ==> bomba
-            if (bombs_index.includes(parseInt(this.innerHTML))) {
-                this.classList.add('bomb');
-                // stampa punteggio
-                const divScore = createDiv('div-score');
-                console.log(clicked_index);
-                divScore.innerHTML = `Il tuo punteggio &egrave;: ${clicked_index.length}`;
-                container.prepend(divScore);
-            } else {
+            if (!bombs_index.includes(parseInt(this.innerHTML) -1) && clicked_index.length <= clicked_index_max_length) {
                 this.classList.add('click');
+                this.classList.add('pointer-events-none');
                 clicked_index.push(i);
+                if (clicked_index.lengthS == clicked_index_max_length) {
+                    divScore = endGame(this, bombs_index, clicked_index, 'Complimenti! Hai Vinto. Punteggio massimo ottenuto: ');
+                    container.prepend(divScore);
+                }
+            } else {
+                // se numero presente in lista generati ==> bomba
+                divScore = endGame(this, bombs_index, clicked_index, 'Hai ottenuto un punteggio pari a ');
+                container.prepend(divScore);
             }
         });
         container.appendChild(square);
     }
 }
-function generateGrid(difficulty) {
-    // attribuisci a row e col valori diversi a seconda della difficoltá
+/**
+ * 
+ * @param {*} difficulty  attuale difficolta del gioco(string: easy-medium-hard)
+ * @returns array[#righe,#colonne]
+ */
+function gridSetup (difficulty) {
     let row;
     let col;
     switch (difficulty) {
@@ -58,42 +91,54 @@ function generateGrid(difficulty) {
         case 'hard':
             row = 7;
             col = 7;
+            // row = 2;
+            // col = 2;
             break;
         default:
             // modifiche impreviste, setta a zero per non creare griglia, non soddisfando la condizione del ciclo for sottostante
             row = 0;
             col = 0;
     }
-    printGrid (row,col);
+    return [row,col];
+}
+/**
+ * 
+ * @param {*} difficulty attuale difficolta del gioco(string: easy-medium-hard)
+ */
+function generateGrid(difficulty) {
+    // attribuisci a row e col valori diversi a seconda della difficoltá
+    row = gridSetup(difficulty)[0];
+    col = gridSetup(difficulty)[1];
+    printGrid(row, col);
 }
 //genera numero casuale tra un min e un max(il numero di celle disponibili)
+/**
+ * 
+ * @param {*} min posizione cella iniziale
+ * @param {*} max posizione cella finale
+ * @returns generazione numero casuale
+ */
 function intGen(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+/**
+ * 
+ * @param {*} difficulty attuale difficolta del gioco(string: easy-medium-hard)
+ * @returns dimensione: righe x colonne
+ */
 function getDim(difficulty) {
-    switch (difficulty) {
-        case 'easy':
-            row = 10;
-            col = 10;
-            break;
-        case 'medium':
-            row = 9;
-            col = 9;
-            break;
-        case 'hard':
-            row = 7;
-            col = 7;
-            break;
-        default:
-            // modifiche impreviste, setta a zero per non creare griglia, non soddisfando la condizione del ciclo for sottostante
-            row = 0;
-            col = 0;
-    }
+    row = gridSetup(difficulty)[0];
+    col = gridSetup(difficulty)[1];
     return row * col;
 }
+/**
+ * 
+ * @returns array contente gli indici delle bombe
+ */
 function generateBomb() {
     // ciclo: genero sempre 16 bombe
     const numBomb = 16;
+    // const numBomb = 0;
     const bombs_index = [];
     for (let i = 0; i < numBomb; i++) {
         const dim = getDim(select.value);
@@ -104,99 +149,27 @@ function generateBomb() {
         }
         bombs_index.push(number);
     }
-    // console.log(bombs_index, 'length: ', bombs_index.length);
+    // console.log(bombs_index);
     return bombs_index;
 }
-// CREAZIONE ELEMENTI DOM
-function createDiv(mainClass) {
-    const div = document.createElement('div');
-    div.classList.add(mainClass);
-    return div;
-}
-function createOption(value) {
-    let option = document.createElement('option');
-    option.value = value;
-    return option;
-}
-function createDom (body) { 
-    // creo header
-    const header = document.createElement ('header');
-    header.classList.add('header');
-    //header-left
-    const headerLeft = createDiv('header-left');
-    // logo
-    const logo = document.createElement('img');
-    let logo_src = 'img/logo-boolean.png';
-    logo.src=logo_src;
-    logo.alt = 'logo di Boolean';
-    logo.id = 'logo';
-    // titolo
-    const title = document.createElement('h1');
-    title.classList.add('title');
-    title.innerHTML = 'Campo Minato';
-    //  header-right
-    const headerRight = createDiv('header-right');
-    // span
-    const span_header = document.createElement('span');
-    span_header.classList.add('difficulty');
-    span_header.innerHTML = 'Difficolt&aacute;: ';
-    // select
-    const select = document.createElement('select');
-    select.name = 'select-difficulty';
-    const optionEasy = createOption('easy');
-    optionEasy.innerHTML = 'easy';
-    // select.append(optionEasy);
-    const optionMedium = createOption('medium');
-    optionMedium.innerHTML = 'medium';
-    // select.append(optionMedium);
-    const optionHard = createOption('hard');
-    optionHard.innerHTML = 'hard';
-    // button
-    const button = document.createElement('button');
-    button.classList.add('btn-play');
-    button.innerHTML = 'Play';
-    
-    //main
-    const main = document.createElement('main');
-    main.classList.add('main');
-    const container = createDiv('container');
-    
-    
-    // footer
-    const footer = document.createElement('footer');
-    footer.classList.add('footer');
-    const copyright = createDiv('copyright');
-    const span_footer = document.createElement('span');
-    span_footer.classList.add('copyright');
-    span_footer.innerHTML = 'Made with <i class="fas fa-heart"></i> by <a href="#" class="subline-link">Boolean</a>';
-    // FINE: ----CREAZIONE ELEMENTI DOM
-    //elementi inseriti in index.html
-    headerLeft.append(logo,title);
-    select.append(optionEasy,optionMedium,optionHard);
-    headerRight.append(span_header,select,button);
-    header.append(headerLeft,headerRight);
-    main.append(container);
-    footer.append(copyright);
-    copyright.append(span_footer);
-    //prepend per mantenere script a fondo pagina
-    body.prepend(header,main,footer);
-    return button;
-}
 
-const body = document.querySelector('body');
+/**
+ * PROGRAMMA
+ *  */
+createDom(); // in altro file
 //ottengo bottone gioco
-let button = createDom(body);
+const button = document.querySelector('.btn-play');
 //azione play
 const select = document.querySelector('select');
 //cambia griglia al click sul bottone
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     //creo griglia a seconda delle dimenzioni
-    switch(select.value) {
+    switch (select.value) {
         default: generateGrid(select.value);
     }
 });
 // cambia griglia al change della select
-select.addEventListener('change',function() {
+select.addEventListener('change', function () {
     switch (select.value) {
         default: generateGrid(select.value);
         // let bombs_index = generateBomb();
